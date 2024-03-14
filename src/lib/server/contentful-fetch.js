554a1fcae081c3c53158ef5532d8cf98f7ac3bef
AlexVslk <myownowlalex@gmail.com>
2024-03-14 17:31:39 +0300

    test

--- src/lib/server/contentful-fetch.js
+++ src/lib/server/contentful-fetch.js
@@ -1,13 +1,11 @@
-import { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } from '$env/static/private'
-
 const contentfulFetch = async (query) => {
-  const url = 'https://graphql.contentful.com/content/v1/spaces/' + CONTENTFUL_SPACE_ID
+  const url = 'https://graphql.contentful.com/content/v1/spaces/' + '7nazxz33277c'
 
   const response = await fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + CONTENTFUL_ACCESS_TOKEN,
+      Authorization: 'Bearer ' + '-go2NZ40hWgEl0Bj2ub3J3obyxT6gZOFegkV028UK9s',
     },
     body: JSON.stringify({ query }),
   })
